753620b31789dedc92faa458de03893751e8f177
Alexis Tremblay <alexis_tunder291@hotmail.com>
2021-11-21 13:45:16 -0500

    Ajouter valeurs par défauts à premiere migration

--- database/migrations/1637077743526_films.ts
+++ database/migrations/1637077743526_films.ts
@@ -7,22 +7,22 @@ export default class Films extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.string('titre')
-      table.string('description')
+      table.string('titre').defaultTo('')
+      table.string('description').defaultTo('')
       //table.integer('genre_id').references('id').inTable('Genre')
-      table.integer('genre_id')
-      table.integer('rating')
-      table.string('img', 511)
-      table.string('acteur')
-      table.string('realisateur')
-      table.integer('duree_minutes')
-      table.date('date_sortie')
-      table.date('date_fin')
+      table.integer('genre_id').defaultTo(0)
+      table.integer('rating').defaultTo(0)
+      table.string('img', 511).defaultTo('')
+      table.string('acteur').defaultTo('')
+      table.string('realisateur').defaultTo('')
+      table.integer('duree_minutes').defaultTo(0)
+      table.date('date_sortie').defaultTo(this.now())
+      table.date('date_fin').defaultTo(this.now())
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
